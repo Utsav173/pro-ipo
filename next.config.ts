b3ff2efa2b6afb7a9332cc4cc78775b584ec6c4e
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2024-12-19 22:55:00 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  sw: "pwabuilder-sw.js",
-});
-
 const nextConfig: NextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;

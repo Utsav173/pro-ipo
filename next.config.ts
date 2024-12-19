a68bc39dce8064da7991d0916866b3862004acd9
Utsav173 <khatriutsav40@gmail.com>
2024-12-19 22:37:20 +0530

    added offline support and sw

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  sw: "pwabuilder-sw.js",
+});
 
 const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
